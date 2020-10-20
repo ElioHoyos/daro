fa086efdeb500b0284872b3dad967a6fc3eb44a7
ElioHoyos <elio18david@gmail.com>
2020-10-20 10:33:54 -0500

    plantilla de la pagina web

---
+++ app/navegacion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class navegacion extends Model
+{
+    //
+}
---
+++ database/factories/PaginaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\pagina;
+use Faker\Generator as Faker;
+
+$factory->define(pagina::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
