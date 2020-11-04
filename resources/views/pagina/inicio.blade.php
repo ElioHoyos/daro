6efde59b9364316f4089c8001171fbf7e86bc243
ElioHoyos <elio18david@gmail.com>
2020-11-03 23:54:29 -0500

    avances

--- resources/views/pagina/inicio.blade.php
+++ resources/views/pagina/inicio.blade.php
@@ -102,26 +102,36 @@
     Intro Section
   ============================-->
   <section id="intro" class="clearfix">
-    <div class="container">
-      <strong><p>Negocios y Servicios DARO SAC</p></strong>
-        <img src="{{asset('img/daro/logo_daro_gif.gif')}}" alt="">
-        <strong><p>DISTRIBUIDOR FERRETERO
-          <br>
-          LIDER EN LA AMAZONIA PERUANA</p></strong>
-      <div class="intro-img">
-        <img src="{{asset('img/intro-img.svg')}}" alt="" class="img-fluid">
+    
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="{{asset('img/slide/banner-ferreteriaaa.png')}}" alt="First slide">
+        </div>
         
-      </div>
-
-      <div class="intro-info">
-        
-        <div>
-          {{-- <a href="#about" class="btn-get-started scrollto">Get Started</a>
-          <a href="#services" class="btn-services scrollto">Our Services</a> --}}
+        <div class="carousel-item">
+          <img class="d-block w-100" src="{{asset('img/slide/ferreteriaaa.png')}}" alt="Second slide">
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+          <h5>elalal</h5>
+          <p>asasas</p>
         </div>
       </div>
-
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
+
   </section><!-- #intro -->
 
   <main id="main">
@@ -822,7 +832,7 @@
               ------ <br>
               -------<br>
              Pucallpa - Perú <br>
-              <strong>Telefono:</strong> 986 135 157<br>
+              <strong>Telefono:</strong> <a href="wa.me/51986135157" target="_blank" title="enviame un mensaje">986 135 157</a> <br>
               <strong>Correo:</strong> info@example.com<br>
             </p>
 
